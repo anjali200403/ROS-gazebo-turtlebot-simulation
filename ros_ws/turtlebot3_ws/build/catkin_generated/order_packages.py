a43e2ec8baa9b708e9c833a2dd01bd9bcb7d0b44
anjali <ag27102003@gmail.com>
2023-12-18 13:16:48 +0530

    my first commit

---
+++ ros_ws/turtlebot3_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/anjali/Desktop/workspace/ros_ws/turtlebot3_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
